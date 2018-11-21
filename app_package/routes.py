24dfbc5cffad11832e99419a0c80d877147bff45
Pawel <p.podbielski86@gmail.com>
2018-11-21 19:50:48 +0100

    template

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app_package import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world'
+    user = {'username': 'Pawel'}
+    return render_template('index.html', title='Home', user=user)
