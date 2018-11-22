4629f18e9a7585cdda812d32ad02b6473f81876f
Pawel Podielski <pawel.podbielski@eurobank.pl>
2018-11-22 15:30:19 +0100

    flask WTF

--- app_package/routes.py
+++ app_package/routes.py
@@ -6,4 +6,15 @@ from app_package import app
 @app.route('/index')
 def index():
     user = {'username': 'Pawel'}
-    return render_template('index.html', title='Home', user=user)
+
+    posts = [
+        {
+            'author': {'username': 'Pawel'},
+            'body': 'Beautiful day in Poland'
+         },
+        {
+            'author': {'username': 'Kasia'},
+            'body': 'Chce ciuchy'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
