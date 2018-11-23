c590d65913a87ad2bb35bebad903aedbaee96492
Pawel Podielski <pawel.podbielski@eurobank.pl>
2018-11-23 15:43:07 +0100

    forms

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app_package import app
 from app_package.forms import LoginForm
 
@@ -21,7 +21,10 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        #flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
